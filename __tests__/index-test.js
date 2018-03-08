5d0251913be2373517859914a5816a7094f4c2e2
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-03-08 11:33:53 -0300

    Add jest and eslint

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,8 +1,8 @@
 import expect from 'expect'
 import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
 
-import Component from 'src/'
+import Component from '../src/'
 
 describe('Component', () => {
   let node
@@ -16,7 +16,7 @@ describe('Component', () => {
   })
 
   it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
+    render(<Component />, node, () => {
       expect(node.innerHTML).toContain('Welcome to React components')
     })
   })
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
 import Example from '../../src'
 
 class Demo extends Component {
   render() {
-    return <div>
+    return (<div>
       <h1>camera-scan Demo</h1>
-      <Example/>
-    </div>
+      <Example />
+    </div>)
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
